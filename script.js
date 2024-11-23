f209bfef4b889f292f79a74b3e743bce1f4dff18
= <ahmedjawad1857@gmail.com>
2024-11-23 19:17:12 +0500

    Maked parralax Scroll effect

---
+++ script.js
@@ -0,0 +1,23 @@
+const listBg = document.querySelectorAll(".banner .bg");
+const titleBanner = document.querySelector(".banner h1");
+const listTab = document.querySelectorAll(".tab");
+
+window.addEventListener("scroll", (event) => {
+  let top = this.scrollY;
+  listBg.forEach((bg, index) => {
+    if (index != 0 && index != 8) {
+      bg.style.transform = `translateY(${(top * index) / 2}px)`;
+    } else if (index == 0) {
+      bg.style.transform = `translateY(${top / 3}px)`;
+    }
+    titleBanner.style.transform = `translateY(${(top * 4) / 2}px)`;
+  });
+  
+  listTab.forEach((tab) => {
+    if (tab.offsetTop - top < 550) {
+      tab.classList.add("active");
+    } else {
+      tab.classList.remove("active");
+    }
+  });
+});
